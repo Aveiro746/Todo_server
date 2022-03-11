6c313f765c283a4f28b0e7c20e72dd614e5c5515
Aveiro746 <95100461+Aveiro746@users.noreply.github.com>
2022-03-11 11:12:57 -0500

    first commit

---
+++ config/mongoConfig.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+exports.mongooseConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewURLParser: true,
+    });
+
+    await mongoose.connection;
+    console.log(`MongoDB Connected`);
+  } catch (err) {
+    const error = new Error(err);
+    console.error(error);
+  }
+}
+
+
